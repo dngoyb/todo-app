8fece1f78afd810120192903227eee6acb176333
Ngoy Banza <dngoyb@gmail.com>
2019-04-26 10:55:38 +0200

    first commit

---
+++ todo-functions.js
@@ -0,0 +1,69 @@
+// Fetch existing todos from localStorage
+const getSavedTodo = function(){
+    const todosJSON = localStorage.getItem('todos')
+
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON)
+    }else{
+        return[]
+    }
+}
+
+// Save todos to localStorage
+const saveTodos = function(todos){
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Render application todos based on filters
+const renderTodos = function(todos, filters){
+    
+    let filterTodo = todos.filter(todo => {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    filterTodo = filterTodo.filter(function (todo) {
+        return !filters.hideCompleted || !todo.completed
+    })
+   
+    const incompleteTodo = todos.filter(todo => {
+        return !todo.completed
+    })
+
+    document.querySelector('#todo').innerHTML = ''
+    
+    document.querySelector('#todo').appendChild(generateSummaryDOM(incompleteTodo))
+
+
+    filterTodo.forEach(todo => {
+        document.querySelector('#todo').appendChild(generateTodoDOM(todo))
+    });
+}
+
+// Get the DOM elements for an individual note
+const generateTodoDOM = function(todo){
+    const todoDiv = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const span = document.createElement('span')
+    const button = document.createElement('button')
+
+    //Set checkbox attribute and add to the div
+    checkbox.setAttribute('type', 'checkbox')
+    todoDiv.appendChild(checkbox)
+
+    //set span content and append to the div
+    span.textContent = todo.text
+    todoDiv.appendChild(span)
+
+    //set button content 
+    button.textContent = 'x'
+    todoDiv.appendChild(button)
+    return todoDiv
+}
+
+// Get the DOM elements for list summary
+const generateSummaryDOM = function(incompleteTodo){
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodo.length} left`
+    return summary
+}
+
---
+++ todos-app.js
@@ -0,0 +1,30 @@
+let todos = getSavedTodo()
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', e => {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', e => {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.name.value,
+        completed: false
+    })
+    saveTodos(todos)
+    renderTodos(todos, filters)
+    e.target.elements.name.value = ''
+})
+
+document.querySelector('#hide-incomplete').addEventListener('change', e =>{
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
+
